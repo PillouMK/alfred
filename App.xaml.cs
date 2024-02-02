883d68de4d456bc52aafe9a4c03efb51935ca59f
PillouMK <maxence.pille@gmail.com>
2024-02-02 18:49:17 +0100

    initial commit for alfred

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Alfred
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
